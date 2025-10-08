8d84145bb8df600cfdc24af555954dfe702615f4
kzmiura <136795515+kzmiura@users.noreply.github.com>
2025-10-08 16:46:37 +0900

    Use stdout lock and implement error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::io;
+use std::io::prelude::*;
 use std::path::{Path, PathBuf};
 
 use clap::Parser;
@@ -27,6 +29,7 @@ fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
     fn visit_dirs(
+        writer: &mut impl Write,
         dir: &Path,
         prefix: &mut Vec<&str>,
         depth: Option<u32>,
@@ -34,49 +37,59 @@ fn main() -> std::io::Result<()> {
     ) -> std::io::Result<Summary> {
         let mut summary = Summary::default();
         if dir.is_dir() && depth.is_none_or(|d| d > 0) {
-            let mut entries = fs::read_dir(dir)?
-                .filter_map(|e| e.inspect_err(|e| eprintln!("{}", e)).ok())
-                // Hidden files
-                .filter(|e| cli.show_hidden || !e.file_name().as_encoded_bytes().starts_with(b"."))
-                .peekable();
-            while let Some(entry) = entries.next() {
-                let path = entry.path();
-                let file_type = entry.file_type()?;
-                let raw_file_name = entry.file_name();
-
-                // Printing
-                let (arm, padding) = if entries.peek().is_some() {
-                    ("+-- ", "|   ")
-                } else {
-                    ("`-- ", "    ")
-                };
-                println!("{}{}{}", prefix.concat(), arm, raw_file_name.display());
-
-                // Post-printing processing
-                if !cli.follow_symlinks && file_type.is_symlink() {
-                    continue;
+            match fs::read_dir(dir) {
+                Err(e) => {
+                    eprintln!("{}: {}", dir.display(), e);
                 }
+                Ok(entries) => {
+                    let mut entries = entries.peekable();
+                    while let Some(entry) = entries.next() {
+                        let entry = entry?;
+                        let path = entry.path();
+                        let file_type = entry.file_type()?;
+                        let raw_file_name = entry.file_name();
+                        
+                        // Pre-printing processing
+                        if !cli.show_hidden && raw_file_name.as_encoded_bytes().starts_with(b".") {
+                            continue;
+                        }
 
-                // Recursion
-                prefix.push(padding);
-                let Summary {
-                    dir_count,
-                    file_count,
-                } = visit_dirs(
-                    &path,
-                    prefix,
-                    depth.map(|d| d - 1),
-                    cli,
-                )?;
-                prefix.pop();
+                        // Printing
+                        let (arm, padding) = if entries.peek().is_some() {
+                            ("+-- ", "|   ")
+                        } else {
+                            ("`-- ", "    ")
+                        };
+                        writeln!(
+                            writer,
+                            "{}{}{}",
+                            prefix.concat(),
+                            arm,
+                            raw_file_name.display()
+                        )?;
 
-                // Summary
-                summary.dir_count += dir_count;
-                summary.file_count += file_count;
-                if file_type.is_dir() {
-                    summary.dir_count += 1;
-                } else if file_type.is_file() {
-                    summary.file_count += 1;
+                        // Post-printing processing
+                        if !cli.follow_symlinks && file_type.is_symlink() {
+                            continue;
+                        }
+
+                        // Recursion
+                        prefix.push(padding);
+                        let Summary {
+                            dir_count,
+                            file_count,
+                        } = visit_dirs(writer, &path, prefix, depth.map(|d| d - 1), cli)?;
+                        prefix.pop();
+
+                        // Summary
+                        summary.dir_count += dir_count;
+                        summary.file_count += file_count;
+                        if file_type.is_dir() {
+                            summary.dir_count += 1;
+                        } else if file_type.is_file() {
+                            summary.file_count += 1;
+                        }
+                    }
                 }
             }
         }
@@ -87,10 +100,12 @@ fn main() -> std::io::Result<()> {
     println!("{}", cli.dir.display());
 
     // Enter the recursion
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
     let Summary {
         dir_count,
         file_count,
-    } = visit_dirs(&cli.dir, &mut vec![], cli.depth, &cli)?;
+    } = visit_dirs(&mut handle, &cli.dir, &mut vec![], cli.depth, &cli)?;
 
     // Print summary
     if cli.show_summary {
